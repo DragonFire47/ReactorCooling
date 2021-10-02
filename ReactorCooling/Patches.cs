b62e7a212efba1c6be46ac4963e136bd791e13dd
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2021-10-02 16:47:46 -0400

    Add project files.

---
+++ ReactorCooling/Patches.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ReactorCooling
+{
+    [HarmonyPatch(typeof(PLAntiFireGrenadeInstance), "Explode")]
+    class GrenadeExplode
+    {
+        private static void Prefix(PLAntiFireGrenadeInstance __instance, ref float ___m_FirefightingEffect_Strength)
+        {
+            if (__instance.MyTLI != null && __instance.MyTLI.MyShipInfo != null)
+            {
+                if (__instance.MyTLI.MyShipInfo.ReactorInstance != null)
+                {
+                    float sqrMagnitude2 = (__instance.MyTLI.MyShipInfo.ReactorInstance.transform.position - __instance.transform.position).sqrMagnitude;
+                    if (sqrMagnitude2 < 18f)
+                    {
+                        __instance.MyTLI.MyShipInfo.MyStats.ReactorTempCurrent -= 150f * ___m_FirefightingEffect_Strength;
+                    }
+                }
+            }
+        }
+    }
+    [HarmonyPatch(typeof(PLAntiFireGrenadeInstance), "Update")]
+    class GrenadePassive
+    {
+        private static void Prefix(PLAntiFireGrenadeInstance __instance, ref float ___m_FirefightingEffect_Strength)
+        {
+            if (__instance.MyTLI != null && __instance.MyTLI.MyShipInfo != null)
+            {
+                if (__instance.MyTLI.MyShipInfo.ReactorInstance != null)
+                {
+                    float sqrMagnitude2 = (__instance.MyTLI.MyShipInfo.ReactorInstance.transform.position - __instance.transform.position).sqrMagnitude;
+                    if (sqrMagnitude2 < 18f)
+                    {
+                        __instance.MyTLI.MyShipInfo.MyStats.ReactorTempCurrent -= Time.deltaTime * 15f * ___m_FirefightingEffect_Strength;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ReactorCooling/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using PulsarPluginLoader;
+
+namespace ReactorCooling
+{
+    public class Plugin : PulsarPlugin
+    {
+        public override string Version => "1.0.0";
+
+        public override string Author => "Dragon";
+
+        public override string ShortDescription => "Adds fire extinguisher and fire-killer grenade cooling back into the game";
+
+        public override string LongDescription => base.LongDescription;
+
+        public override string Name => "ReactorCooling";
+
+        public override string HarmonyIdentifier()
+        {
+            return "Dragon.ReactorCooling";
+        }
+    }
+}
